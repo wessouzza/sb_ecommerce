4cc17954176e1b83dc452cc57d8bb2ed90182603
wessouzza <wes-souza@hotmail.com>
2024-02-12 19:17:30 -0300

    Feat: Security

---
+++ src/main/java/com/ecommerce/shoppy/dto/AuthenticationDto.java
@@ -0,0 +1,5 @@
+package com.ecommerce.shoppy.dto;
+
+public record AuthenticationDto(String username, String password) {
+    
+}
---
+++ src/main/java/com/ecommerce/shoppy/services/AuthorizationService.java
@@ -0,0 +1,23 @@
+package com.ecommerce.shoppy.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.ecommerce.shoppy.repositories.UserRepository;
+
+@Service
+public class AuthorizationService implements UserDetailsService{
+    private UserRepository userRepository;
+
+    public AuthorizationService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username);
+    }
+    
+}
